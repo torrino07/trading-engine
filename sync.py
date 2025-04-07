5f493ef972c909c61d413a3804d79d7d54eb7ecf
DGtalledo <61323407+torrino07@users.noreply.github.com>
2025-04-07 21:39:03 +0200

    test

--- sync.py
+++ sync.py
@@ -1,48 +1,47 @@
+from datetime import datetime
 from pathlib import Path
 import os
-import shutil
-import subprocess
-
-AWS_REGION="us-east-1"
-S3_PATH="marketdata001-dev/binlogs"
 
 def aws_sync_s3(directory):
-    os.environ["AWS_DEFAULT_REGION"] = AWS_REGION
-    result = subprocess.run(["aws", "s3", "sync", directory, "s3://" + S3_PATH, "--exact-timestamps"], capture_output=True, text=True)
-    print("Sync successful" if result.returncode == 0 else f"Sync failed\n{result.stderr}")
+    response = os.system(f"aws s3 sync {directory} s3://{S3_PATH} --exact-timestamps --region {AWS_REGION}")
+    print("Sync successful" if response == 0 else "Sync failed")
 
 def filter_files(directory):
-    biggest_hour, biggest_minute = -1, -1
+    biggest_hour, lowest_biggest_minute = -1, -1
     
-    array = []
-    for root, dirs, files in os.walk(directory):
-        path_parts = root.split("/")
-        if len(path_parts) >= 9:
-            array.append(path_parts[-1])
+    for root, _, _ in os.walk(directory):
+        parts = root.split("/")
+        if len(parts) >= 9:
             try:
-                hour, minute = int(path_parts[-2]), int(path_parts[-1])
-                if hour > biggest_hour or (hour == biggest_hour and minute > biggest_minute):
-                    biggest_hour, biggest_minute = hour, minute
+                hour, minute = int(parts[-2]), int(parts[-1])
+                if hour > biggest_hour or (hour == biggest_hour and minute > lowest_biggest_minute):
+                    biggest_hour, lowest_biggest_minute = hour, minute
             except ValueError:
                 continue
-    
-    minute_count = len(sorted(set(array)))
-    if minute_count > 2:
+    if lowest_biggest_minute > 2:
+        date = datetime.now().strftime("%Y-%m-%d")
         files = sorted(list(map(str, Path(directory).rglob("*.binlog"))))
-        return [f for f in files if f"{biggest_hour}/{biggest_minute}" not in f]
+        return [f for f in files if f"/{date}/{biggest_hour}/{lowest_biggest_minute}/" not in f]
     else:
         return None
 
-# 1. Filter Files
-filtered_files = filter_files("./binlogs")
+def main():
+    filtered_files = filter_files("./binlogs")
+    
+    if filtered_files != None:
+        for file in filtered_files:
+            dest_path = os.path.join("staging", os.path.relpath(file, "binlogs"))
+            dest_dir = os.path.dirname(dest_path)
+            os.makedirs(dest_dir, exist_ok=True)
+            try:
+                os.rename(file, dest_path)
+                print(f"Moved {file} to {dest_path}")
+            except OSError as e:
+                print(f"Failed to move {file} to {dest_path}: {e}")
 
-# 2. Move to staging dir
-if filtered_files != None:
-    for file in filtered_files:
-        dest_path = os.path.join("staging", os.path.relpath(file, "binlogs"))
-        dest_dir = os.path.dirname(dest_path)
-        os.makedirs(dest_dir, exist_ok=True)
-        shutil.move(file, dest_path)
+        aws_sync_s3("./staging")
 
-# 3. Sync with AWS S3
-    aws_sync_s3("./staging")
+if __name__ == "__main__":
+    AWS_REGION="us-east-1"
+    S3_PATH="marketdata001-dev/binlogs"
+    main()
