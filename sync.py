09b4936897bee2aa94eaa18c2f38c97022fe6310
DGtalledo <61323407+torrino07@users.noreply.github.com>
2025-04-08 22:43:43 +0200

    test

--- sync.py
+++ sync.py
@@ -1,47 +1,28 @@
-from datetime import datetime
 from pathlib import Path
+from datetime import datetime, timedelta
 import os
 
-def aws_sync_s3(directory):
-    response = os.system(f"aws s3 sync {directory} s3://{S3_PATH} --exact-timestamps --region {AWS_REGION}")
+def aws_sync_s3():
+    response = os.system(f"aws s3 sync ./staging s3://{S3_BUCKET}/binlogs/staging --exact-timestamps --region {AWS_REGION}")
     print("Sync successful" if response == 0 else "Sync failed")
 
-def filter_files(directory):
-    biggest_hour, lowest_biggest_minute = -1, -1
-    
-    for root, _, _ in os.walk(directory):
-        parts = root.split("/")
-        if len(parts) >= 9:
-            try:
-                hour, minute = int(parts[-2]), int(parts[-1])
-                if hour > biggest_hour or (hour == biggest_hour and minute > lowest_biggest_minute):
-                    biggest_hour, lowest_biggest_minute = hour, minute
-            except ValueError:
-                continue
-    if lowest_biggest_minute > 2:
-        date = datetime.now().strftime("%Y-%m-%d")
-        files = sorted(list(map(str, Path(directory).rglob("*.binlog"))))
-        return [f for f in files if f"/{date}/{biggest_hour}/{lowest_biggest_minute}/" not in f]
-    else:
-        return None
-
 def main():
-    filtered_files = filter_files("./binlogs")
-    
-    if filtered_files != None:
-        for file in filtered_files:
-            dest_path = os.path.join("staging", os.path.relpath(file, "binlogs"))
+    format = "%Y-%m-%d/%H/%M/"
+    current_date_time_obj = datetime.now() - timedelta(hours=2)
+    current_date_time = current_date_time_obj.strftime(format)
+    next_date_time = (current_date_time_obj + timedelta(minutes=1)).strftime(format)
+    for path in sorted(list(map(str, Path("./binlogs").rglob("*.binlog")))):
+        if current_date_time not in path and next_date_time not in path:
+            dest_path = os.path.join("staging", os.path.relpath(path, "binlogs"))
             dest_dir = os.path.dirname(dest_path)
             os.makedirs(dest_dir, exist_ok=True)
-            try:
-                os.rename(file, dest_path)
-                print(f"Moved {file} to {dest_path}")
-            except OSError as e:
-                print(f"Failed to move {file} to {dest_path}: {e}")
-
-        aws_sync_s3("./staging")
+            os.rename(path, dest_path)
+            print(dest_path, dest_dir)
+    aws_sync_s3()
+    os.system("ls")
+    os.system("rm -rf staging")
 
 if __name__ == "__main__":
     AWS_REGION="us-east-1"
-    S3_PATH="marketdata001-dev/binlogs"
+    S3_BUCKET="marketdata001-dev"
     main()
